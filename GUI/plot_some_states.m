e1ae48e0529c249fb71d99345dc459f7a5672d96
mkrtl <30895867+mkrtl@users.noreply.github.com>
2017-08-16 10:25:11 +0200

    Update: Comments and improvement
    File is commented and legend entries are ordered and displayed properly.

--- GUI/plot_some_states.m
+++ GUI/plot_some_states.m
@@ -1,5 +1,4 @@
-% There are the following global variables: indexes_used country_names.
-% GUI has not been commented yet.
+% There are the following global variables: indexes_used, wbd_data.
 
 function varargout = plot_some_states(varargin)
 % PLOT_SOME_STATES MATLAB code for plot_some_states.fig
@@ -22,7 +21,6 @@ function varargout = plot_some_states(varargin)
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
-kount = 0;
 % Edit the above text to modify the response to help plot_some_states
 
 % Last Modified by GUIDE v2.5 15-Aug-2017 14:29:32
@@ -60,6 +58,14 @@ handles.output = hObject;
 % Update handles structure
 guidata(hObject, handles);
 
+% Run generate_all_countries just once in the beginning and make it
+% accessible for all functions in this file with the 'global' command.
+% Other functions therefore do not have to run generate_all_countries 
+% anymore:
+
+global wbd_data
+generate_all_countries;
+
 % UIWAIT makes plot_some_states wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
@@ -90,16 +96,25 @@ function listbox1_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to listbox1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
-generate_all_countries;
+
+% Access wbd_data:
+
+global wbd_data
+
+% Fill listbox with names of wbd_data:
+
 listbox_names = cell(1,length(wbd_data));
 for i = 1 : length(wbd_data)
     listbox_names(i) = cellstr(wbd_data(i).country);
 end
-k = 0;
-    
+
+% Set listbox object with names of wbd_data:
+
 set(hObject,'String',listbox_names);
+
 % Hint: listbox controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
+% Default lines:
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -110,7 +125,10 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-generate_all_countries;
+
+% Access wbd_data:
+global wbd_data
+
 axes(handles.axes1);
 x_values = linspace(0,1,1000);
 A = get(handles.listbox1,'String');
@@ -119,16 +137,30 @@ index = find_index(wbd_data,string(name));
 
 
 global indexes_used
-global country_names
+
+
+% Indexes of wbd_data used:
 indexes_used(end+1) = index;
-    
+
+% Name of the countries plotted:
 country_names = strings(length(indexes_used),1);
+
+
+% Fill array of country_names:
 
 for i = 1 : length(indexes_used)
     country_names(i) = wbd_data(indexes_used(i)).country;
 end
-indexes_used = unique(indexes_used);
-country_names = unique(country_names);
+
+% Make sure, that every country is just plotted once, meaning it just
+% appears once in indexes_used and country_names. Keep order of those
+% arrays ( which is achieved by 'stable'):
+
+indexes_used = unique(indexes_used,'stable')
+country_names = unique(country_names,'stable')
+
+% Plot datapoints and Lorenz curve seperately:
+
 for i = 1 : length(country_names)
     epsilon = wbd_data(indexes_used(i)).epsilon;
     curves(i) = plot(x_values,mixed_lorenz(x_values,epsilon,0.6));
@@ -136,17 +168,23 @@ for i = 1 : length(country_names)
     datapoints(i) = plot(wbd_data(indexes_used(i)).share_pop,wbd_data(indexes_used(i)).cumulated_dist_vector,'+');
     hold on
 end
+
+% Check for 'Datapoints'-checkbox and decide whether or not to display
+% them:
 if ~get(handles.checkbox1,'Value')
     set(datapoints,'Visible','Off')
 else
     set(datapoints,'Visible','On')
-    
 end     
     
 hold off
 for i = 1 : length(country_names)
+    
     legend_entries(2*i-1) = country_names(i);
+    % Legend entries of Lorenz Curves
+    
     legend_entries(2*i) = join(["Daten ",country_names(i)]);
+    % Legend entries of datapoints
 end
 
 legend(legend_entries,'Location','Northwest')
@@ -158,14 +196,17 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
 global indexes_used
 indexes_used = [];
-global country_names
-country_names = [];
-global count
-count = 0;
+
+
+% Clear arrays, which indicate which countries were plotted.
+
 cla(handles.axes1);
+% Clear canvas, but keep options.
 guidata(hObject,handles);
+% Update GUI
 
 
 % --- Executes on button press in checkbox1.
@@ -176,6 +217,11 @@ function checkbox1_Callback(hObject, eventdata, handles)
 
 %Hint: get(hObject,'Value') returns toggle state of checkbox1
 pushbutton1_Callback(hObject, eventdata, handles);
+
+% Change of checkbox initiates a new plot, but with different settings
+% regarding the datapoints. The last country is actually plotted once
+% again, which is eliminated by the unique-Statement in
+% Pushbutton1_Callback.
 
 
 
