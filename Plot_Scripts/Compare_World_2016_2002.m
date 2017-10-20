bffae4d2fb34cb9beba542335c8723e0297b655e
mkrtl <30895867+mkrtl@users.noreply.github.com>
2017-10-20 13:22:15 +0200

    Update Figure_7

--- Plot_Scripts/Compare_World_2016_2002.m
+++ Plot_Scripts/Compare_World_2016_2002.m
@@ -1,13 +1,13 @@
 % Plot Welt 2002
 generate_all_countries;
 World = common_distribution(wbd_data,"World");
-Welt_2002;
+Welt_2002_kaempke_neu = Welt_2002();
 x_grid = 0:0.001:1;
 plot(World.share_pop,World.cumulated_dist_vector,'.',...
     x_grid,mixed_lorenz(x_grid,find_epsilon_simple(World,1000,0.6),0.6),...
     Welt_2002_kaempke_neu.share_pop,Welt_2002_kaempke_neu.cumulated_dist_vector,'.',...
     x_grid,mixed_lorenz(x_grid,find_epsilon_simple(Welt_2002_kaempke_neu,1000,0.6),0.6))
-str = "Welt 2016";
+str = "World 2016";
 dim_1 = [.15 .53 0 0];
 box_1 = annotation('textbox',dim_1,'String',str, 'FitBoxToText','on', 'Interpreter','none','FontName','Helvetica');
 box_1.FontSize = 12;
@@ -19,7 +19,7 @@ box_2 = annotation('textbox',dim_2,'String',epsilon_str, 'FitBoxToText','on');
 box_2.FontSize = 12;
 box_2.LineStyle = 'None';
 
-str = "Welt 2002";
+str = "World 2002";
 dim_3 = [.15 .44 0 0];
 box_3 = annotation('textbox',dim_3,'String',str, 'FitBoxToText','on', 'Interpreter','none','FontName','Helvetica');
 box_3.FontSize = 12;
