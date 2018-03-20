04c9781a4f5f9efb69c63b686fdd2a4cd1e6903b
mkrtl <30895867+mkrtl@users.noreply.github.com>
2018-03-20 10:33:50 +0100

    Update 20/03/18

--- Plot_Functions/plot_mixed_mult_data.m
+++ Plot_Functions/plot_mixed_mult_data.m
@@ -1,34 +1,44 @@
-function y=plot_mixed_mult_data(a,varargin)
+function y=plot_mixed_mult_data(states)
 
 % Plots Pareto Lorenz curve of multiple countries, while using
 % the epsilon calculated by the Gini-value of the World Bank.
 % Function is able to plot multiple objects in the same plot. Function is
 % function for Equity objects.
-n_states=length(varargin);  
-varargin{1}.share_pop
+a = 0.6;
+n_states=length(states);  
 for i=1:n_states
-    eps = varargin{i}.epsilon;
+    eps = states(i).epsilon;
     f=@(x) a* (1-(1-x).^eps) + (1-a) * x.^(1/eps);
-    varargin{i}.share_pop
-    y=plot(0:0.001:1,f(0:0.001:1),varargin{i}.share_pop,varargin{i}.cumulated_dist_vector,'+','DisplayName',varargin{i}.country)
+    states(i).share_pop
+    y=plot(0:0.001:1,f(0:0.001:1),states(i).share_pop,states(i).cumulated_dist_vector,'+','DisplayName',states(i).country)
         
     hold on
 end 
-legend('Income Data World Bank','Mixed Lorenz Curve','Location','northwest');
 xlabel('Cumulative Population Share')
 ylabel('Cumulative Income Share')
 
-str = join([varargin{1}.country,string(varargin{1}.year_of_data)]);
-dim_1 = [.15 .53 0 0];
-box_1 = annotation('textbox',dim_1,'String',str, 'FitBoxToText','on', 'Interpreter','none','FontName','Helvetica');
-box_1.FontSize = 12;
-box_1.LineStyle = 'None';
-
-str = join(["G = ",string(round(varargin{1}.gini_data,2))]);
-dim_2 = [.15 .49 0 0];
-box_2 = annotation('textbox',dim_2,'String',str, 'FitBoxToText','on', 'Interpreter','none','FontName','Helvetica');
-box_2.FontSize = 12;
-box_2.LineStyle = 'None';
+text_legend = [];
+box_size = 0.09;
+for i = 1 : n_states
+    
+    str = join([states(i).country,string(states(i).year_of_data)]);
+    height = 0.53-(i-1)*box_size;
+    dim_1 = [.15 height 0 0];
+    box_1 = annotation('textbox',dim_1,'String',str, 'FitBoxToText','on', 'Interpreter','none','FontName','Helvetica');
+    box_1.FontSize = 12;
+    box_1.LineStyle = 'None';
+    
+    str = join(["G = ",string(round(states(i).gini_data,2))]);
+    height_2 = height - 0.04;
+    dim_2 = [.15 height_2 0 0];
+    box_2 = annotation('textbox',dim_2,'String',str, 'FitBoxToText','on', 'Interpreter','none','FontName','Helvetica');
+    box_2.FontSize = 12;
+    box_2.LineStyle = 'None';
+    
+    text_legend = [text_legend,strcat("Income Data World Bank ",states(i).country),strcat("Standard Lorenz Curve ",states(i).country)];
+end
+legend('show','Location','northwest');
+legend(text_legend);
 
 grid on
 hold off
